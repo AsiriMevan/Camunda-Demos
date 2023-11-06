ea2faf0d991e9dcc573e91477ef892b529c12c58
Mevan Senanayake <mevan.senanayake@digiratina.com>
2023-11-06 17:52:07 +0530

    first commit

---
+++ Pizza-Order/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+y
+Isrc/main/java/org/camunda/bpm/getstarted/pizza/PizzaOrderApplication.java,3\f\3f2de1ba354fd314fbe30043e0e82f3e96265763
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ Pizza-Order/src/main/java/org/camunda/bpm/getstarted/pizza/PizzaOrderApplication.java
@@ -0,0 +1,12 @@
+package org.camunda.bpm.getstarted.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaOrderApplication.class, args);
+    }
+
+}
